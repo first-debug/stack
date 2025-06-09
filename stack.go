9d7bf8aaf371699438e955f07636500a1c144dd3
vladi <vladislavvasilev53@gmail.com>
2025-06-09 14:29:24 +0300

    дописал базу

---
+++ stack.go
@@ -0,0 +1,28 @@
+package stack
+
+type Stack[T any] struct {
+	i     int
+	slice []T
+}
+
+func NewStack[T any]() Stack[T] {
+	return Stack[T]{
+		i:     -1,
+		slice: make([]T, 10),
+	}
+}
+
+func (stack *Stack[T]) pop() (res T) {
+	if stack.i >= 0 {
+		res = stack.slice[stack.i]
+		stack.i--
+	} else {
+		panic("Стек пуст!")
+	}
+	return
+}
+
+func (stack *Stack[T]) push(val T) {
+	stack.slice = append(stack.slice, val)
+	stack.i++
+}
